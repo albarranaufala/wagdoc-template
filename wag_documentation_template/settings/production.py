a24ce9f0b2ee35ba31ac2ee80f523eef42a917fe
Albarra Naufala <albarraerdanto@gmail.com>
2025-05-14 16:37:12 +0700

    Update configuration files: enhance .env.example, refine production settings, and adjust requirements

--- wag_documentation_template/settings/production.py
+++ wag_documentation_template/settings/production.py
@@ -1,6 +1,12 @@
+from decouple import config
+
 from .base import *
 
-DEBUG = False
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+SECRET_KEY = config("SECRET_KEY", default="")
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost").split(",")
 
 try:
     from .local import *
